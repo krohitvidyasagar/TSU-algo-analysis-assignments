76ddb96e47c7d1434a150f5f915d37d00c5f28a8
krohitvidyasagar <k.rohitvidyasagar@gmail.com>
2022-10-05 12:50:32 -0500

    Adding quick sort changes

--- assignment-4-quick-sort/QuickSort.java
+++ assignment-4-quick-sort/QuickSort.java
@@ -37,8 +37,7 @@ public class QuickSort {
         arr[j] = temp;
     }
 
-    static int partition(int[] arr, int begin, int end) {
-        int temp;
+    static int partitionLastElementPivot(int[] arr, int begin, int end, int size) {
         int pivot = arr[end];
 
         int i = begin - 1;
@@ -51,26 +50,80 @@ public class QuickSort {
         }
 
         swap(arr, i + 1, end);
+
+        System.out.println("After partition");
+        printArray(arr, size);
+        System.out.println("");
+
         return (i + 1);
     }
 
-    static void quickSort(int[] arr, int begin, int end) {
+    static void quickSortLastElementPivot(int[] arr, int begin, int end, int size) {
         if (begin < end) {
-            int pi = partition(arr, begin, end);
+            int pi = partitionLastElementPivot(arr, begin, end, size);
 
-            quickSort(arr, begin, pi-1);
+            quickSortLastElementPivot(arr, begin, pi-1, size);
 
-            quickSort(arr, pi+1, end);
+            quickSortLastElementPivot(arr, pi+1, end, size);
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 1, 5, 8, 9, 2, 3, 6, 11};
+        QuickSort qs = new QuickSort();
 
-        int arr_size = arr.length;
+        // Worst Case Arrays
+        // Sorted in ascending order of length = 14
+        int[] arr1 = {15, 22, 31, 45, 66, 70, 82, 91, 102, 111, 125, 140, 178, 195};
 
-        quickSort(arr, 0, arr_size - 1);
+        // Sorted in descending order of length = 23
+        int[] arr2 = {310, 300, 290, 273, 251, 242, 226, 208, 170, 150, 90, 81, 65, 44, 30, 21, 19, 11, 8, 5};
 
-        printArray(arr, arr_size);
+        System.out.println("Original first array");
+        printArray(arr1, arr1.length);
+
+        System.out.println("");
+        quickSortLastElementPivot(arr1, 0, arr1.length - 1, arr1.length);
+
+        System.out.println("Sorted first array");
+        printArray(arr1, arr1.length);
+
+        System.out.println("===========================================================================================");
+
+        System.out.println("Original second array");
+        printArray(arr2, arr2.length);
+
+        System.out.println("");
+        quickSortLastElementPivot(arr2, 0, arr2.length - 1, arr2.length);
+
+        System.out.println("Sorted second array");
+        printArray(arr2, arr2.length);
+
+        System.out.println("===========================================================================================");
+
+
+        // Quick sort for random array
+        int j = 1;
+        int[] randomArray = new int[30]; 
+        int randomArrayLength;
+
+        while (j < 4) {
+            randomArrayLength = qs.getRandomNumber(10, 20);
+
+            randomArray = qs.getInitializedRandomArray(randomArray, randomArrayLength);
+
+
+            System.out.println("Original random array: " + j);
+            printArray(randomArray, randomArrayLength);
+    
+            System.out.println("");
+            quickSortLastElementPivot(randomArray, 0, randomArrayLength - 1, randomArrayLength);
+    
+            System.out.println("Sorted random array: " + j);
+            printArray(randomArray, randomArrayLength);
+    
+            System.out.println("===========================================================================================");
+            
+            j++;
+        }
     }
 }
