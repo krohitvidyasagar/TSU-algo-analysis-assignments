f9032613ad5af6c355c5525438a2acc5700826ec
krohitvidyasagar <k.rohitvidyasagar@gmail.com>
2022-10-06 10:28:36 -0500

    Quick sort table adde

--- assignment-4-quick-sort/QuickSort.java
+++ assignment-4-quick-sort/QuickSort.java
@@ -33,22 +33,24 @@ public class QuickSort {
     }
 
     static void swap(int[] arr, int i, int j)
+    // Swap two elements of an array of index i and j
     {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
-    static int partitionLastElementPivot(int[] arr, int begin, int end, int size, int[] counter) {
+    static int partitionWithLastElementAsPivot(int[] arr, int begin, int end, int size, int[] counter) {
+        // Perform partition of an array with last element as pivot
         int pivot = arr[end];
 
         int i = begin - 1;
 
         for (int j = begin; j <= end - 1; j++) {
-            counter[0]++;
             if (arr[j] < pivot) {
                 i++;
                 swap(arr, i, j);
+                counter[0]++;
             }
         }
 
@@ -62,17 +64,19 @@ public class QuickSort {
         return (i + 1);
     }
 
-    static void quickSortLastElementPivot(int[] arr, int begin, int end, int size, int[] counter) {
+    static void quickSortUsingLastElementAsPivot(int[] arr, int begin, int end, int size, int[] counter) {
+        // Quick sort using last element as pivot
         if (begin < end) {
-            int pi = partitionLastElementPivot(arr, begin, end, size, counter);
+            int pi = partitionWithLastElementAsPivot(arr, begin, end, size, counter);
 
-            quickSortLastElementPivot(arr, begin, pi-1, size, counter);
+            quickSortUsingLastElementAsPivot(arr, begin, pi-1, size, counter);
 
-            quickSortLastElementPivot(arr, pi+1, end, size, counter);
+            quickSortUsingLastElementAsPivot(arr, pi+1, end, size, counter);
         }
     }
 
     static int hoarePartition(int[] arr, int begin, int end, int size, int[] counter) {
+        // Perform partition using Hoare
         int pivot = arr[begin];
         int i = begin - 1, j = end + 1;
  
@@ -90,18 +94,23 @@ public class QuickSort {
             } while (arr[j] > pivot);
  
             // If two pointers met.
-            if (i >= j)
+            if (i >= j) {
+                System.out.println("After partition");
+                printArray(arr, size);
+                System.out.println("");
+                
                 return j;
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            // swap(arr[i], arr[j]);
+            }
+            
+            counter[0]++;
+            swap(arr, i, j);
         }
             
     }
 
 
     static void hoareQuickSort(int[] arr, int begin, int end, int size, int[] counter) {
+        // Perform quick sort using Hoare
         if (begin < end) {
             int pi = hoarePartition(arr, begin, end, size, counter);
 
@@ -114,9 +123,7 @@ public class QuickSort {
     public static void main(String[] args) {
         QuickSort qs = new QuickSort();
 
-        Map<Integer, Integer> inputToActualCountMap = new HashMap<>();
-
-        int[] counter = {0};
+        Map<Integer, Integer> inputToActualCountMapWorstCaseArray = new HashMap<>();
 
         // Worst Case Arrays
         // Sorted in ascending order of length = 14
@@ -128,10 +135,14 @@ public class QuickSort {
         System.out.println("Original first array");
         printArray(arr1, arr1.length);
 
-        System.out.println("");
-        quickSortLastElementPivot(arr1, 0, arr1.length - 1, arr1.length, counter);
+        int[] counter = {0};
 
-        inputToActualCountMap.put(arr1.length, counter[0]);
+        System.out.println("");
+        quickSortUsingLastElementAsPivot(arr1, 0, arr1.length - 1, arr1.length, counter);
+
+        System.out.println("Array length: " + arr1.length + " Counter: " + counter[0]);
+
+        inputToActualCountMapWorstCaseArray.put(arr1.length, counter[0]);
 
         System.out.println("Sorted first array");
         printArray(arr1, arr1.length);
@@ -144,9 +155,11 @@ public class QuickSort {
         counter[0] = 0;
 
         System.out.println("");
-        quickSortLastElementPivot(arr2, 0, arr2.length - 1, arr2.length, counter);
+        quickSortUsingLastElementAsPivot(arr2, 0, arr2.length - 1, arr2.length, counter);
 
-        inputToActualCountMap.put(arr2.length, counter[0]);
+        System.out.println("Array length: " + arr2.length + " Counter: " + counter[0]);
+
+        inputToActualCountMapWorstCaseArray.put(arr2.length, counter[0]);
 
         System.out.println("Sorted second array");
         printArray(arr2, arr2.length);
@@ -154,11 +167,12 @@ public class QuickSort {
         System.out.println("===========================================================================================");
 
 
-        // Quick sort for random array
         int j = 1;
         int[] randomArray = new int[30]; 
         int randomArrayLength;
+        Map<Integer, Integer> inputToActualCountMapRandomArray = new HashMap<>();
 
+        // Quick sort for random array
         while (j < 4) {
             counter[0] = 0;
             randomArrayLength = qs.getRandomNumber(10, 20);
@@ -169,9 +183,11 @@ public class QuickSort {
             printArray(randomArray, randomArrayLength);
     
             System.out.println("");
-            quickSortLastElementPivot(randomArray, 0, randomArrayLength - 1, randomArrayLength, counter);
+            quickSortUsingLastElementAsPivot(randomArray, 0, randomArrayLength - 1, randomArrayLength, counter);
 
-            inputToActualCountMap.put(randomArrayLength, counter[0]);
+            System.out.println("Array length: " + randomArrayLength + " Counter: " + counter[0]);
+
+            inputToActualCountMapRandomArray.put(randomArrayLength, counter[0]);
     
             System.out.println("Sorted random array: " + j);
             printArray(randomArray, randomArrayLength);
@@ -180,5 +196,57 @@ public class QuickSort {
             
             j++;
         }
+
+        int k = 1;
+        Map<Integer, Integer> inputToActualCountMapHoarePartition = new HashMap<>();
+
+        // Quick sort for random array using Hoare partition
+        while(k < 3) {
+            counter[0] = 0;
+            randomArrayLength = qs.getRandomNumber(10, 20);
+
+            randomArray = qs.getInitializedRandomArray(randomArray, randomArrayLength);
+
+            System.out.println("Original random array: " + j);
+            printArray(randomArray, randomArrayLength);
+    
+            System.out.println("");
+            hoareQuickSort(randomArray, 0, randomArrayLength - 1, randomArrayLength, counter);
+
+            inputToActualCountMapHoarePartition.put(randomArrayLength, counter[0]);
+    
+            System.out.println("Sorted random array: " + k);
+            printArray(randomArray, randomArrayLength);
+    
+            System.out.println("===========================================================================================");
+            k++;
+        }
+
+        System.out.println("\n Quick sort for worst case arrays table\n");
+
+        System.out.println(String.format("%10s %25s %10s %23s %10s", "N", "|", "Actual Count", "|", "T(N) = (n * n)"));
+        System.out.println(String.format("%s", "-------------------------------------------------------------"));
+
+        inputToActualCountMapWorstCaseArray.forEach((input, count) -> {
+            System.out.println(String.format("%10d %25s %10d %25s %10d", input, "|", count, "|", (input * input)));
+        });
+
+        System.out.println("\n Quick sort for random arrays table\n");
+
+        System.out.println(String.format("%10s %25s %10s %23s %10s", "N", "|", "Actual Count", "|", "T(N) = (n logn)"));
+        System.out.println(String.format("%s", "-------------------------------------------------------------"));
+
+        inputToActualCountMapRandomArray.forEach((input, count) -> {
+            System.out.println(String.format("%10d %25s %10d %25s %10d", input, "|", count, "|", (input *  (int) (Math.log(input) / Math.log(2)))));
+        });
+
+        System.out.println("\n Quick sort using Hoare Partition table\n");
+
+        System.out.println(String.format("%10s %25s %10s %23s %10s", "N", "|", "Actual Count", "|", "T(N) = (n logn)"));
+        System.out.println(String.format("%s", "-------------------------------------------------------------"));
+
+        inputToActualCountMapHoarePartition.forEach((input, count) -> {
+            System.out.println(String.format("%10d %25s %10d %25s %10d", input, "|", count, "|", (input * (int) (Math.log(input) / Math.log(2)))));
+        });
     }
 }
